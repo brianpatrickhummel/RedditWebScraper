65e73e7083ceec7ef2f342bd8ddada801d997df4
brianpatrickhummel <frankgucci@mac.com>
2017-08-19 16:14:03 -0500

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mongooseCheerio
+
+
+## Heroku mLab - Mongo URI
+mongodb://heroku_52ft34g8:dt67ql648ors7978n91vie8k03@ds149603.mlab.com:49603/heroku_52ft34g8
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Scraping
+var request = require("request");
+var cheerio = require("cheerio");
+// Models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+// Serve static content for the app from the 'public' directory in the application directory.
+app.use(express.static('public'));
+// Morgan Logging
+app.use(logger("dev"));
+// Body-Parser Config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Hanldebars Config
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Set Mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+// Database configuration with mongoose
+var uristring = "mongodb://heroku_52ft34g8:dt67ql648ors7978n91vie8k03@ds149603.mlab.com:49603/heroku_52ft34g8" || 
+                "mongodb://localhost/mongoosecheerio"
+mongoose.connect(uristring);
+
+var db = mongoose.connection;
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// Routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function () {
+  console.log('Listening on Port ' + PORT);
+});
