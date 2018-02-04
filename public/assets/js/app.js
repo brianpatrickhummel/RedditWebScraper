88f8cea83948971a95bf257e66bf44a4d2cd5733
brianpatrickhummel <frankgucci@mac.com>
2018-02-03 22:55:49 -0800

    revisited app today, discovered but in operation.  all scraped articles were being saved immediately upon scrape, rather than being saved only when user clicked the save article button.  revised app.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,66 +1,69 @@
 // If any article from database is displayed on screen, delete the Prompt to Scrape
+
+let articles = [];
 if (document.querySelector(".mongoArticles")) {
   $(".emtpyPlaceholder").remove();
 }
 
 // CircleType.js
-var headerArc = new CircleType(document.getElementById("headerArc")).radius(
-  1800
-);
-var headerArc2 = new CircleType(document.getElementById("headerArc2")).radius(
-  1800
-);
+var headerArc = new CircleType(document.getElementById("headerArc")).radius(1800);
+var headerArc2 = new CircleType(document.getElementById("headerArc2")).radius(1800);
 
-//*************** Button click to Intialize Scraping ***************
-$(document).on("click", ".scrapeButton", function () {
+//*************** Button click to Initialize Scraping ***************
+$(document).on("click", ".scrapeButton", function() {
   $.ajax({
     method: "GET",
     url: "/scrape"
-  }).done(function () {
+  }).done(function(articles) {
     window.location.href = "/";
   });
 });
 
 //*************** Button click to Save an Article ***************
-$(document).on("click", ".saveButton", function () {
+$(document).on("click", ".saveButton", function() {
   var thisId = $(this).attr("id");
+  $(this)
+    .closest(".mongoArticles")
+    .remove();
   $.ajax({
     method: "POST",
     url: "/save/" + thisId
-  }).done(function () {
+  }).done(function() {
     // refreshes the page and makes a GET call for all unsaved articles
     window.location.href = "/";
   });
 });
 
 //*************** Button click to Delete an Article ***************
-$(document).on("click", ".deleteButton", function () {
-  var thisId = $(this).attr("id");
+$(document).on("click", ".deleteButton", function() {
+  var thisId = $(this).attr("data-id");
+  var articleKey = $(this).attr("id");
   $.ajax({
     method: "DELETE",
-    url: "/delete/" + thisId
-  }).done(function () {
+    url: "/delete/" + thisId + "/" + articleKey
+  }).done(function() {
     // refreshes the page and makes a GET call for all saved articles
-    window.location.href = "/savedArticles";
+    window.location.reload(true);
   });
 });
 
 //*************** Button click to View/Add/Delete Notes ***************
 // When the Bootstrap modal is triggered...
-$("#addNoteModal").on("show.bs.modal", function (event) {
+$("#addNoteModal").on("show.bs.modal", function(event) {
   // Removes the existing DOM Modal panels from previous query
   $(".articleNotes").remove();
   // Identify Button that triggered the modal
   var button = $(event.relatedTarget);
+  console.log(button);
   // Extract article id from data-id attribute
   var articleId = button.data("id");
-  // console.log("client-side: clicked button id= " + articleId);
+  console.log("client-side: clicked button id= " + articleId);
 
   // Get a single article including it's notes
   $.ajax({
     method: "GET",
     url: "/singleArticleNotes/" + articleId
-  }).done(function (result) {
+  }).done(function(result) {
     // jQuery update the Bootstrap Modal
     for (var i = 0; i < result.note.length; i++) {
       var jsonReturn = JSON.stringify(result.note[i]);
@@ -90,7 +93,7 @@ $("#addNoteModal").on("show.bs.modal", function (event) {
     }
 
     // *************** Button click to Save a Note ***************
-    $(document).on("click", ".saveNoteButton", function () {
+    $(document).on("click", ".saveNoteButton", function() {
       $.ajax({
         method: "PUT",
         url: "/saveNote/" + articleId,
@@ -98,7 +101,7 @@ $("#addNoteModal").on("show.bs.modal", function (event) {
           // Value taken from note textarea
           body: $("#comment").val()
         }
-      }).done(function () {
+      }).done(function() {
         // clear the typed note
         $("#comment").val("");
         window.location.href = "/savedArticles";
@@ -106,13 +109,13 @@ $("#addNoteModal").on("show.bs.modal", function (event) {
     });
 
     // *************** Button click to Delete a Note ***************
-    $(".deleteNoteButton").on("click", function () {
+    $(".deleteNoteButton").on("click", function() {
       var deleteId = $(this).attr("id");
       console.log("Note ID to be deleted: " + deleteId);
       $.ajax({
         method: "DELETE",
         url: "/deleteNote/" + deleteId + "/" + articleId
-      }).done(function () {
+      }).done(function() {
         window.location.href = "/savedArticles";
       });
     });
